3b883d16d7447bd13b0f35db9861f5290c9ab6af
cschackerl <christian.schackerl@immobilienscout24.at>
2015-07-16 13:52:48 +0200

    add ginding itemsets with parameters, matching with contains instead of exact matching

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,129 +10,57 @@ object Application extends Controller {
   val sourceXml : String = new java.io.File(".").getCanonicalPath() + "/app/assets/Rules.xml"  //"C:/github/just-play-scala/app/assets/Rules.xml"
   val rawXml = scala.xml.XML.loadFile(sourceXml)
   val sourceItems: Items = Items.fromEntireXml(rawXml)
+  val sourceItemsets: Itemsets = Itemsets.fromEntireXml(rawXml)
 
-  class Rule(var id: Integer, var value: String) {
+  class Rule(var id: Int, var value: String) {
     override def toString =
       s"id: $id, value: $value"
   }
   object Rule
 
-  class Item(var id: String, var value: String) {
-    override def toString =
-      s"id:$id, value:$value"
+  class Item(var id: Int, var value: String) {
+    override def toString = s"id:$id, value:$value"
   }
   object Item
 
   class Items(val allItems: Seq[Item]){
     override def toString = (for (v <- allItems) yield sys.props("line.separator") + v.id + ": " + v.value).toString()
-    //def getItemIds(values: ListBuffer[String]):Seq[Char] = allItems.filter(i => values.filter(p => i.value == p).length > 0).flatMap(_.id)
-    def getItemIds(values: ListBuffer[String]):Seq[String] = allItems.filter(i => values.exists(p => i.value == p)).map(_.id)
+    def getItemIds(values: ListBuffer[String]):Seq[Int] = allItems.filter(i => values.exists(p => i.value == p)).map(_.id)
   }
   object Items {
     // convert XML to an Item object
     def fromEntireXml(node: scala.xml.Node):Items = {
-      val allItems = (node \\ "PMML" \\ "AssociationModel" \\ "Item" ).map( x => new Item((x \\ "@id").toString(),(x \ "@value").toString()))
+      val allItems = (node \\ "PMML" \\ "AssociationModel" \\ "Item" ).map( x => new Item((x \\ "@id").toString.toInt,(x \ "@value").toString()))
       new Items(allItems)
     }
   }
 
-  class Itemset(var id: String, var itemRefs: Seq[String]) {
-    override def toString =
-      s"id: $id, itemRefs: $itemRefs.toString()"
+  class Itemset(var id: Int, var itemIds: Seq[Int]) {
+    override def toString = s"id: $id, itemRefs: $itemsets.toString()"
   }
   object Itemset
 
   class Itemsets(var itemsets: Seq[Itemset]){
     override def toString = (for (is <- itemsets) yield sys.props("line.separator") + "ItemSetId:" + is.id + ": " + {
-      for(itemRef <- is.itemRefs) yield itemRef.toString()
+      for(itemId <- is.itemIds) yield itemId.toString()
     }).toString()
+
+    //def getItemsetIds(itemIds: Seq[Int]):Seq[Int] = (itemsets.filter(is => itemIds.toSet.subsetOf(is.itemIds.toSet)).map(_.id))
+    def getItemsetIds(itemIds: Seq[Int]):Seq[Int] = (itemsets.filter(is => itemIds forall (is.itemIds contains)).map(_.id))
+    //def getItemsetIds(itemIds: Seq[Int]):Seq[Int] = itemsets.map(_.id)
   }
   object Itemsets {
 
     // convert XML to an Item object
     def fromEntireXml(node: scala.xml.Node):Itemsets = {
       val itemsets = (node \\ "PMML" \\ "AssociationModel" \\ "Itemset" )
-        .map( x => new Itemset((x \\ "@id").toString(),(x \\ "ItemRef").map(iref =>(x \\ "@itemRef").toString())))
+        .map( x => new Itemset((x \\ "@id").toString.toInt,(x \\ "ItemRef").map(iref =>(iref \\ "@itemRef").toString().toInt)))
       new Itemsets(itemsets)
     }
   }
 
-
-  case class SearchParameter(id: Int, name: String)
-    
-  val searchParameters = List(
-        SearchParameter(1, "Fabio"),
-        SearchParameter(2, "Reinhard"),
-        SearchParameter(3, "Christian")
-  )
-
-    //implicit val placesWrites = Json.writes[SearchParameter]
-    
-  def items = Action {
-        
-        //val json = Json.toJson(searchParameters)
-        //Ok(json)
-        //val rawXml = scala.xml.XML.loadFile(sourceXml)
-        
-        // find what i want
-        //var items: List[String] = List("apples", "oranges", "pears")
-       
-        //GUT
-        //val value = (rules \\ "PMML" \\ "AssociationModel" \\ "Item" \\ "@value" )
-        //val value = (rules \\ "PMML" \\ "AssociationModel" \\ "Item" \\ "@value" ).map(_.text).mkString(" ")
-
-
-      //val value = (rules \\ "PMML" \\ "AssociationModel" \\ "Item" \\ "@value")
-        //val new = value.foreach( item => value)
-        //.foreach( item =>
-        //   items = items ::: List(item)  )
-           
-        //Ok(items.toString)
-        //val value2 = (rules \\ "PMML" \\ "AssociationModel" \  "@functionName") text
-     
-        
-        //Ok((rules \\ "PMML" \\ "AssociationModel" ))
-        
-        
-        
-        //val x = <div class="content"><p>Hello</p><p>world</p></div>
-        //val result = (x \\ "p")
-        //val result2 =for (n <- x.child) yield n.text
-        
-        //val a = Array(1, 2, 3, 4, 5)
-        
-        
-        //Ok(for (e <- a) yield e)
-        //Ok(value.toString)
-
-
-      //-------------
-      //http://alvinalexander.com/scala/serializing-deserializing-xml-scala-classes
-
-
-      //var value = (rules \\ "PMML" \\ "AssociationModel" \\ "Item" \\ "@value" ).map(_.text).mkString(" ")
-      // value = value +  (rules \\ "PMML" \\ "AssociationModel" \\ "Item" \\ "@id" ).map(_.text).mkString(" ")
-      //val value = rules.attributes("version")
-
-
-      //var values = (rules \\ "PMML" \\ "AssociationModel" \\ "Item" \\ "@value" ).map(_.text)
-      //var ids = (rules \\ "PMML" \\ "AssociationModel" \\ "Item" \\ "@id" ).map(_.text)
-
-      //Get all Items
-      //var value = (rules \\ "PMML" \\ "AssociationModel" \\ "Item" ).map( x => List((x \\ "@id"),(x \ "@value")))
-      //val result =(for (v <- value) yield v.head.toString() + ": " + v(1).toString()).toString()
-
-      //val result = new Item("3", "juhu test")
-      //val result = Items.fromEntireXml(rawXml)
-      Ok(sourceItems.toString)
-  }
-
-  def itemsets = Action {
-    //val rawXml = scala.xml.XML.loadFile(sourceXml)
-    val result = Itemsets.fromEntireXml(rawXml)
-    Ok(result.toString)
-  }
-
+  def items = Action { Ok(sourceItems.toString)  }
+  def itemsets = Action {  Ok(sourceItemsets.toString) }
   def rules = TODO
 
   def index = Action {
@@ -142,23 +70,20 @@ object Application extends Controller {
   def getNewSearch(hasTerrace: Boolean, hasLift: Boolean) = Action {
     //map parameters to item values/ids
     val input: ListBuffer[String] = ListBuffer()
-    if(hasTerrace) {
-      input += "Terrasse=terrasse:ja"
-    }
-    if(hasLift) {
-      input += "Lift=lift:ja"
-    }
+    if(hasTerrace) input += "Terrasse=terrasse:ja"
+    if(hasLift)  input += "Lift=lift:ja"
+
 
     //find items and get there ids
     val itemIds = sourceItems.getItemIds(input)
 
-
     //find itemsets with this parameters
+    val itemsetIds = sourceItemsets.getItemsetIds(itemIds)
     //find rules and get the consequent
     //get items from itemsets and return it
 
     //return result
-    Ok(itemIds.toString)
+    Ok(itemsetIds.toString)
     //Ok(input.toString)
   }
 }
